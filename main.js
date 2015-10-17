2efa13fd43391192b48b424fc17189f78ec4c5ea
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-10-18 00:34:45 +0900

    Done

--- main.js
+++ main.js
@@ -3,16 +3,15 @@
 
   var colors = ['#ff8e8e', '#ff8ec6', '#ff8eff', '#c68eff', '#8e8eff', '#8ec6ff', '#8effff', '#8effc6', '#8eff8e', '#c6ff8e', '#ffff8e', '#ffc68e'];
 
-  function ColourN(n) {
+  function colourN(n) {
     return colors[n%colors.length];
   }
 
   var memo_fib = [1, 1];
 
-  function Fibo(n) {
-    var self = this;
+  function fibo(n) {
     return memo_fib[n] || function () {
-      memo_fib[n] = self.fib(n-1) + self.fib(n-2);  
+      memo_fib[n] = fibo(n-1) + fibo(n-2);  
       return memo_fib[n];
     }();
   }
@@ -29,12 +28,84 @@
     return (b/(a+b) * 100);
   };
 
+  Vue.component('fibo-base', {
+    template: '#base-template',
+    data: function () {
+      return {n: 5};
+    },
+    computed: {
+      width: function () {
+        var a = fibo(this.n);
+        var b = fibo(this.n-1);
+        return a/(a+b) * 100;
+      },
+      color: function () { return colourN(this.n); },
+    },
+    ready: function () {
+      console.log(this);
+    },
+  });
+
   Vue.component('fibo-a', {
     template: '#a-template',
     props: ['n'],
     methods: {
-      fib: Fibo,
-      colorn: ColourN,
+      fib: fibo,
+      colorn: colourN,
+    },
+    computed: {
+      a: aFunc,
+      b: bFunc,
+
+      color: function () { return this.colorn(this.n); },
+    },
+    ready: function () {
+      console.log(this);
+    }
+  });
+
+  Vue.component('fibo-b', {
+    template: '#b-template',
+    props: ['n'],
+    methods: {
+      fib: fibo,
+      colorn: colourN,
+    },
+    computed: {
+      a: aFunc,
+      b: bFunc,
+
+      color: function () { return this.colorn(this.n); },
+    },
+    ready: function () {
+      console.log(this);
+    }
+  });
+
+  Vue.component('fibo-c', {
+    template: '#c-template',
+    props: ['n'],
+    methods: {
+      fib: fibo,
+      colorn: colourN,
+    },
+    computed: {
+      a: aFunc,
+      b: bFunc,
+
+      color: function () { return this.colorn(this.n); },
+    },
+    ready: function () {
+      console.log(this);
+    }
+  });
+
+  Vue.component('fibo-d', {
+    template: '#d-template',
+    props: ['n'],
+    methods: {
+      fib: fibo,
+      colorn: colourN,
     },
     computed: {
       a: aFunc,
@@ -50,7 +121,7 @@
   var app = new Vue({
     el: '#vue-main',
     data: {
-      n: 3,
+      n: 2,
     },
     ready: function () {
       console.log(this);
